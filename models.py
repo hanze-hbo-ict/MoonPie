dab6b41db75c70e7bc20aa666414d327cb1649cb
Bart Barnard <b.barnard@pl.hanze.nl>
2026-03-30 17:04:09 +0200

    eindelijk die auth wat moderner gemaakt...

--- models.py
+++ models.py
@@ -1,64 +1,108 @@
+from datetime import datetime
+from typing import List
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, Table, Text, select
+from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, String, Date, Float, Text
-from sqlalchemy import Column, Table, ForeignKey
-from sqlalchemy import select
-from sqlalchemy.orm import relationship, Session
+from flask_login import UserMixin
+engine = create_engine("sqlite:///minerals.sqlite3", echo=True)
 
-base = declarative_base()
-engine = create_engine('sqlite:///moonpie.sqlite')
+class Base(DeclarativeBase):
+    pass
 
-class Mineral(base):
-   __tablename__='minerals'
-   id = Column(Integer, primary_key=True)
-   name = Column(String, nullable=False)
-   mollmass = Column(Float)
-   formula = Column(String)
-   description = Column(Text)
-   price = Column(Float)
+class Mineral(Base):
+    __tablename__ = 'minerals'
 
-user_bestellingen = Table('user_bestelling', base.metadata,
-     Column('user_id', ForeignKey('user.id'), primary_key=True),
-     Column('bestelling_id', ForeignKey('bestelling.id'), primary_key=True)
-)
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(30))
+    mollmass:Mapped[float] = mapped_column(Float)
+    formula:Mapped[str] = mapped_column(String(30))
+    description:Mapped[str] = mapped_column(Text)
+    price:Mapped[float] = mapped_column(Float)
 
-class User(base):
-   __tablename__='user'
-   id = Column(Integer, primary_key=True)
-   name = Column(String, nullable=False)
-   password = Column(String, nullable=False)
-   email = Column(String, nullable=False)
+    @classmethod
+    def find_all(cls):
+        result = []
+        with Session(engine) as session:
+            stmt = select(cls)
+            for mineral in session.scalars(stmt):
+                result.append(mineral)
+            
+        return result
 
-   bestellingen = relationship("Bestelling", secondary=user_bestellingen, 
-                               back_populates='users')
-   
-   def save(self):
-      with Session(engine) as session:
-         session.add(self)
-         session.commit()
 
-   
-class Bestelling(base):
-   __tablename__ = 'bestelling'
-   id = Column(Integer, primary_key=True)
-   datum = Column(Date)
-   aantal = Column(Integer)
 
-   users = relationship("User", secondary=user_bestellingen, 
-                               back_populates='bestellingen')
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str]= mapped_column(String(50))
+    email:Mapped[str]= mapped_column(String(50))
+    password:Mapped[str]= mapped_column(String(50))
+    
+    bestellingen:Mapped[List['Bestelling']] = relationship()
+    is_active = True
 
-data = [ ('diopside', 216.55, 'MgCaSi_2O_6', 'Diopside is a monoclinic pyroxene mineral. It forms variably colored, but typically dull green crystals in the monoclinic prismatic class. It has two distinct prismatic cleavages, typical of the pyroxene series. It is transparent to translucent with indices of refraction.', 1.2),
-    ('feldspar', 818.76, 'KAlSi_3O_8 – NaAlSi_3O_8 – CaAl_2Si_2O_8', "Feldspars are a group of rock-forming aluminium tectosilicate minerals, containing sodium, calcium, potassium or barium. The most common members of the feldspar group are the plagioclase (sodium-calcium) feldspars and the alkali feldspars. Feldspars make up about 60% of the Earth's crust.", 2.4),
-    ('ilmenite', 151.71, 'FeTiO_3', 'Ilmenite is a titanium-iron oxide mineral. It is a weakly magnetic black or steel-gray solid. It is the most important ore of titanium and the main source of titanium dioxide.', 3.14),
-    ('olvine',   203.77, '(Mg,Fe)2SiO4', "Olivine is a magnesium iron silicate with a complex chemical formula. The primary component of the Earth's upper mantle, it is a common mineral in Earth's subsurface, but weathers quickly on the surface.", 4.12) ]
+    def get_id(self):
+        return self.id
+
+    def save(self):
+        with Session(engine) as session:
+            session.add(self)
+            session.commit()
+            session.refresh(self)
+
+    def __str__(self):
+        return f'User({self.name=} <{self.email=}>, {self.id=}'
+
+    @classmethod
+    def find(cls, credentials):
+        with Session(engine) as session:
+           user = session.query(User).where(User.name == credentials['u_name']).first()
+
+           if user is None:
+              return False
+    
+        return True
+
+    @classmethod
+    def get_user_by_name(cls, username):
+        with Session(engine) as session:
+            user = session.query(User).where(
+                User.name == username).first()
+            return user
+
+    @classmethod
+    def get_user_by_id(cls, id):
+        with Session(engine) as session:
+            user = session.query(User).where(
+                User.id == id).first()
+            return user
+
+
+class Bestelling(Base):
+    __tablename__ = 'bestellingen'
+    user_id:Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
+    mineral_id:Mapped[int] = mapped_column(ForeignKey('minerals.id'), primary_key=True)
+    datum:Mapped[datetime] = mapped_column(DateTime)
+    aantal:Mapped[int] = mapped_column(Integer)
+
 
 if __name__=='__main__':
-   base.metadata.create_all(engine)
-   
-   session = Session(engine)
-   for x in data:
-      min = Mineral(name=x[0], mollmass=x[1], formula=x[2], description=x[3], price=x[4] )
-      session.add(min)
+    data = [ ('diopside', 216.55, 'MgCaSi_2O_6', 'Diopside is a monoclinic pyroxene mineral. It forms variably colored, but typically dull green crystals in the monoclinic prismatic class. It has two distinct prismatic cleavages, typical of the pyroxene series. It is transparent to translucent with indices of refraction.', 1.2),
+        ('feldspar', 818.76, 'KAlSi_3O_8 – NaAlSi_3O_8 – CaAl_2Si_2O_8', "Feldspars are a group of rock-forming aluminium tectosilicate minerals, containing sodium, calcium, potassium or barium. The most common members of the feldspar group are the plagioclase (sodium-calcium) feldspars and the alkali feldspars. Feldspars make up about 60% of the Earth's crust.", 2.4),
+        ('ilmenite', 151.71, 'FeTiO_3', 'Ilmenite is a titanium-iron oxide mineral. It is a weakly magnetic black or steel-gray solid. It is the most important ore of titanium and the main source of titanium dioxide.', 3.14),
+        ('olvine',   203.77, '(Mg,Fe)2SiO4', "Olivine is a magnesium iron silicate with a complex chemical formula. The primary component of the Earth's upper mantle, it is a common mineral in Earth's subsurface, but weathers quickly on the surface.", 4.12) ]
 
-   session.commit()
+    Base.metadata.create_all(engine)
+    # with Session(engine) as session:
+    #     for x in data:
+    #         mineral = Mineral(name=x[0], mollmass=x[1], formula=x[2], description=x[3], price=x[4] )
+    #         print(mineral)
+    #         session.add(mineral)
+    #     session.commit()
+
+    print(User.get_user_by_name('piet'))
 
--- server.py
+++ server.py
@@ -6,9 +6,11 @@ from wtforms import (StringField, BooleanField, DateTimeField,
 from wtforms.validators import DataRequired
 
 from flask_login import LoginManager
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
-import database as db
+import auth
+from models import Mineral
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'demoapplicatie'
@@ -19,12 +21,17 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_user_by_id(user_id)
+    #print(username)
+    return auth.get_user_by_id(user_id)
+
+# @login_manager.request_loader
+# def request_loader(request):
+    return auth.get_user(request.form.get('u_name'))
 
 
 class LoginForm(FlaskForm):
     u_name = StringField("Gebruikersnaam")
-    u_password = PasswordField("Wachtwoord")
+    u_pass = PasswordField("Wachtwoord")
 
 class OnboardingForm(FlaskForm):
     u_name = StringField("Gebruikersnaam")
@@ -49,7 +56,7 @@ def logout():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = db.check_user(dict(request.form))
+        user = auth.get_user_by_credentials(dict(request.form))
         if (user):
             login_user(user)
             flash ('welkom terug bro')
@@ -61,23 +68,23 @@ def login():
 
 
 @app.route('/aanmelden', methods=['get', 'post'])
-def wtf_aanmelden():
+def aanmelden():
     onboarding = OnboardingForm()
     if onboarding.validate_on_submit():
-        user = db.save_user(dict(request.form))
+        user = auth.save_user(dict(request.form))
         if user:
             flash('aangemeld hoor, alles check')
             return redirect(url_for('bestellen'))
         else:
             flash('er ging iets mis bro')
 
-    return render_template('wtf_aanmelden.html', form=onboarding)
+    return render_template('aanmelden.html', form=onboarding)
 
 
 @app.route('/bestellen', methods=['get'])
 @login_required
 def bestellen():
-    minerals = db.get_minerals()
-    return render_template('better_product_page.html', data=minerals)
+    minerals = Mineral.find_all()
+    return render_template('better_product_page.html', data=minerals, user=current_user)
 
 app.run(debug=True)
